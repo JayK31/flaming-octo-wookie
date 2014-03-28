6a5b71edbc5b4d4ab425c84b7d61193f7ca8cdd3
JayK31 <jay.kaye31@gmail.com>
2014-03-28 14:28:13 -0400

    create app

---
+++ trip_collaborator_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TripCollaboratorApp::Application.config.secret_key_base = 'e2c842ab2e41270099c6a026c7e468978ff832ebaa69c48c46d83ca40c3229cd81c282f23e27aa658188659b076d015b75d69d4a4358df6fa0b4386a17f2929f'
