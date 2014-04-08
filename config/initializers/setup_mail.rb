678adc28e8fbf41391aba9c1ecb7b255dd01f6ea
JayK31 <jay.kaye31@gmail.com>
2014-04-08 14:33:48 -0400

    added welcome mail function

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,11 +3,11 @@ ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
 
 ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
+  :address              => "smtp.mailgun.org",
   :port                 => 587,  
-  :domain               => "gmail.com",  
-  :user_name            => "jay.kaye31@gmail.com",  
-  :password             => "Piazza31!*",  
+  :domain               => "mailgun.org",  
+  :user_name            => "postmaster@sandbox7faf12111e7e441597def2664bbd21d9.mailgun.org",  
+  :password             => "34s7h4ei2x34",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 } 
