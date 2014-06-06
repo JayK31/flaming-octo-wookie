453e3b052e115098c938f67eac1a279afb536977
JayK31 <jay.kaye31@gmail.com>
2014-06-05 23:46:56 -0400

    more styling to user page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,12 +10,16 @@ $(document).ready(function() {
       $("<h3>").appendTo(".trips")
             .text("My Trips")
       $.each(data.trips, function(index, trip) {
+        var url = "/trips/" + trip.id
         $("<section>").appendTo(".trips")
-            .append($("<p>").text(trip.start));
+            .append($("<p>").text(trip.start))
+                .wrap('<a href=' + url + '>');
       })
       $.each(data.invites, function(index, invite) {
+        var url = "/trips/" + invite[0].id
         $("<section>").appendTo(".trips")
-            .append($("<p>").text(invite[0].start));
+            .append($("<p>").text(invite[0].start))
+                .wrap('<a href=' + url + '>');
       })
     })
   })
